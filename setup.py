41f573a0cf46693e0cd03a6eeac37d22e1211866
Mathieu Djobet <mathieudjobet@live.fr>
2021-12-20 22:40:28 +0100

    save files

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+    requirements = [x.strip() for x in content]
 
 setup(name='test770',
       description="first package",
-      packages=["test770","otherpackage"])
+      packages=find_packages,
+      requires=requirements,
+      scripts=['scripts/batch_run'])
